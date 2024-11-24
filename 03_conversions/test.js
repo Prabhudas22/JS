043c3044bd3c31262e3751ec8117e3c6bd11c358
Prabhudas22 <54720254+Prabhudas22@users.noreply.github.com>
2024-11-24 20:49:20 +0000

    conversion commit

---
+++ 03_conversions/test.js
@@ -0,0 +1,16 @@
+// let score = "33"
+// console.log(typeof(score))
+// let valueNumber = Number(score)
+// console.log(typeof(valueNumber))
+
+
+// let bool = ""
+// console.log(typeof(bool))
+
+// let boolean = Boolean(bool)
+// console.log(boolean)
+
+let someNum = 33
+let thatNum = String(someNum)
+console.log(typeof(someNum))
+console.log(typeof(thatNum))
