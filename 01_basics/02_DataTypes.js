d259cda1a811f7568f9bfa18f5c60f6e708d16af
Prabhudas22 <54720254+Prabhudas22@users.noreply.github.com>
2024-12-02 20:30:39 +0000

    DataType Commit

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,17 @@
+// Primitive
+//  7 Types: String, Numbers, Boolean, undefined, Symbol, BigInt
+
+// Reference Type (Non Primitive)
+// Array, Objects, Functions 
+
+let myObj = {
+    name : 'Prabhu',
+    age : 22
+}
+let myFunc = function(){
+    console.log("Hwllo");
+}
+
+console.log(typeof(myObj));
+console.log(typeof(myFunc));
+ 
