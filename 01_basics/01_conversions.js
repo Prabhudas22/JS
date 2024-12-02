3fe28bc37f22424791bad329e16d5871cfac5707
Prabhudas22 <54720254+Prabhudas22@users.noreply.github.com>
2024-12-02 20:10:47 +0000

    New Commit

---
+++ 01_basics/01_conversions.js
@@ -0,0 +1,46 @@
+// let score = "33"
+// console.log(typeof(score))
+// let valueNumber = Number(score)
+// console.log(typeof(valueNumber))
+
+
+// let bool = ""
+// console.log(typeof(bool))
+
+// let boolean = Boolean(bool)
+// console.log(boolean)
+
+// let someNum = 33
+// let thatNum = String(someNum)
+// console.log(typeof(someNum))
+// console.log(typeof(thatNum))
+
+// ******************* Operations *****************************
+
+let value = 3
+let negVal = -value
+//console.log(negVal)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+
+let str1 = "hello "
+let str2 = "prabhu"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+
+
+console.log(null < 1);
+
+ 
+
+
