c3b743e1a57593fe9604ff967a75ccdd89d8bfea
Prabhudas22 <54720254+Prabhudas22@users.noreply.github.com>
2024-12-03 19:22:03 +0000

    num commit

---
+++ 01_basics/04_num.js
@@ -0,0 +1,27 @@
+const num1 = 100
+const num2 = new Number(200)
+
+// console.log(num2.toString());
+//console.log(num1.toFixed(2));
+const otherNum1 = num1.toFixed(2);
+//console.log(typeof(otherNum1)) 
+//console.log(typeof(num1)) 
+
+//console.log(num1.MAX_VALUE);
+
+//++++++++++++++++++MATHS++++++++++++++++++++++++++++
+ 
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(7.6));
+console.log(Math.random()); // Always number from 0 to 1 
+console.log(Math.random()*10); 
+console.log(Math.floor((Math.random()*10) + 1));
+
+const min = 10
+const max = 20
+
+
+
+
+ 
