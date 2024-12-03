60252d80ac3278b45d9c75741af0b5271f4b41ea
Prabhudas22 <54720254+Prabhudas22@users.noreply.github.com>
2024-12-03 19:43:13 +0000

    Date Committed

---
+++ 01_basics/05_Dates.js
@@ -0,0 +1,11 @@
+ let date = new Date()
+
+ console.log(date);//2024-12-03T19:37:17.760Z
+ console.log(date.toString());//Tue Dec 03 2024 19:37:17 GMT+0000 (Coordinated Universal Time)
+ console.log(date.toDateString());//Tue Dec 03 2024
+ console.log(date.toISOString());//2024-12-03T19:37:17.760Z
+ console.log(date.toJSON());//2024-12-03T19:37:17.760Z
+ console.log(date.toLocaleDateString());//12/3/2024
+ console.log(date.toLocaleString());//12/3/2024, 7:37:17 PM
+ console.log(date.toLocaleTimeString());//7:37:17 PM
+ 
